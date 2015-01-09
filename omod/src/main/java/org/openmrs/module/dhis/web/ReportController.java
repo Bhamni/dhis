9f803667076a4ce100116f61cccf6c2d58b96962
Mujir <mujir@thoughtworks.com>
2015-01-09 17:27:34 +0530

    Mujir, Mihir |Ok

--- omod/src/main/java/org/openmrs/module/dhis/web/ReportController.java
+++ omod/src/main/java/org/openmrs/module/dhis/web/ReportController.java
@@ -1,22 +1,16 @@
 package org.openmrs.module.dhis.web;
 
-import aggregatequeryservice.runqueries;
+import aggregatequeryservice.dblog;
 import org.hibernate.SessionFactory;
-import org.openmrs.api.context.Context;
-import org.openmrs.module.dhis.DhisConstants;
-import org.openmrs.module.dhis.mapper.QueryParametersMapper;
-import org.openmrs.module.dhis.model.QueryParameters;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Controller
 public class ReportController extends BaseRestController {
@@ -29,13 +23,10 @@ public class ReportController extends BaseRestController {
     public ReportController() {
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = baseUrl + "fireQueries")
+    @RequestMapping(method = RequestMethod.GET, value = baseUrl + "tasks")
     @ResponseBody
-    public Integer run(@RequestBody QueryParameters queryParameters) {
-        HashMap<String, String> queryParams = QueryParametersMapper.map(queryParameters);
+    public String getAllTasks() {
         DataSource dataSource = SessionFactoryUtils.getDataSource(sessionFactory);
-        Object result = runqueries.AQS(Context.getAdministrationService().getGlobalProperty(DhisConstants.AQS_CONFIG_GLOBAL_PROPERTY_KEY), dataSource, queryParams);
-        HashMap<String, Object> resultFuture = (HashMap<String, Object>) result;
-        return (Integer) resultFuture.get("task_id");
+        return (String) new dblog().getAllTasks(dataSource);
     }
 }
