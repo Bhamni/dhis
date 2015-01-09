d890c5c16d0e2397f0acb82286768161f200dfa4
mihirk <mihir.khatwani@gmail.com>
2015-01-09 14:51:21 +0530

    Mujir, Mihir | Serious business

---
+++ api/src/main/java/org/openmrs/module/dhis/activator/DhisActivator.java
@@ -0,0 +1,35 @@
+package org.openmrs.module.dhis.activator;
+
+import org.openmrs.module.ModuleActivator;
+
+public class DhisActivator implements ModuleActivator {
+    @Override
+    public void willRefreshContext() {
+
+    }
+
+    @Override
+    public void contextRefreshed() {
+
+    }
+
+    @Override
+    public void willStart() {
+
+    }
+
+    @Override
+    public void started() {
+
+    }
+
+    @Override
+    public void willStop() {
+
+    }
+
+    @Override
+    public void stopped() {
+
+    }
+}
