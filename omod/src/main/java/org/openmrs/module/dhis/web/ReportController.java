ac0773631fdf3074c9fae1cb87ff74f0d1d4c3a0
Mujir <mujir@thoughtworks.com>
2015-01-13 17:10:32 +0530

    Mujir | exposing getTaskById through rest endpoint

--- omod/src/main/java/org/openmrs/module/dhis/web/ReportController.java
+++ omod/src/main/java/org/openmrs/module/dhis/web/ReportController.java
@@ -2,13 +2,15 @@ package org.openmrs.module.dhis.web;
 
 import aggregatequeryservice.dblog;
 import com.mchange.v2.c3p0.C3P0Registry;
+import com.mchange.v2.c3p0.PooledDataSource;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Controller
 public class ReportController extends BaseRestController {
@@ -16,10 +18,21 @@ public class ReportController extends BaseRestController {
 
     @RequestMapping(method = RequestMethod.GET, value = baseUrl + "tasks")
     @ResponseBody
-    public String getAllTasks() {
+    public String getAllTasks() throws PropertyVetoException {
         dblog dblog = new aggregatequeryservice.dblog();
         // TODO : Mujir - a better way to get datasource without relying on C3P0.
-        Object allTasks = dblog.getAllTasks((DataSource) (C3P0Registry.getPooledDataSources().toArray()[0]));
+        PooledDataSource dataSource = (PooledDataSource) (C3P0Registry.getPooledDataSources().toArray()[0]);
+        Object allTasks = dblog.getAllTasks(dataSource);
+        return allTasks.toString();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = baseUrl + "task/{taskId}")
+    @ResponseBody
+    public String getTaskById(@PathVariable("taskId") int taskId) throws PropertyVetoException {
+        dblog dblog = new aggregatequeryservice.dblog();
+        // TODO : Mujir - a better way to get datasource without relying on C3P0.
+        PooledDataSource dataSource = (PooledDataSource) (C3P0Registry.getPooledDataSources().toArray()[0]);
+        Object allTasks = dblog.getTaskById(dataSource, taskId);
         return allTasks.toString();
     }
 }
